5d70c3bbfdf86e376c2388b1dedee3130ea831d6
Jonathan Glanz <jonathan.glanz@gmail.com>
2019-12-13 16:43:27 -0500

    package.json auto-update save

--- scripts/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-yarn build 
-yarn test 
-yarn version --patch --non-interactive 
-cp package.json lib/package.json 
-cd lib
-
-yarn publish --no-git-tag-version --no-commit-hooks  --non-interactive 
