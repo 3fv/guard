2975e394290c383ff9f31cd7b9d95c1a0171c506
Jonathan Glanz <jonathan.glanz@gmail.com>
2020-09-23 18:28:28 -0400

    wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,13 @@
 
 set -e
 
-if [[ ! -e index.d.ts ]];then
- mkdir -p lib && cp package.json lib && tsc -b tsconfig.json $@
+if [[ ! -e index.d.ts ]]; then
+  if [[ -d lib ]]; then
+    echo echo "$PWD/lib cleaned"
+    rm -Rf lib
+  fi
+  mkdir -p lib
+  cp package.json lib
+  tsc -b tsconfig.json "$@"
+  echo "$PWD/lib successfully built"
 fi
