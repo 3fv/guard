936d5390f4db6c46f127b6df5c058bcc6dafbaad
Jonathan Glanz <jonathan.glanz@gmail.com>
2021-10-26 21:02:16 -0400

    added test profiles

--- src/test/Guards.spec.ts
+++ src/test/Guards.spec.ts
@@ -23,3 +23,19 @@ test(`isArray`,() => {
 	expect(isArray("1")).toBeFalsy()
 	expect(isArray(["1",2])).toBeTruthy()
 })
+
+test(`instanceOf`, () => {
+	class Test1 {}
+
+	class Test2 extends Test1 {}
+
+
+	let a = new Test1()
+	let b = new Test2()
+	expect(a).toBeInstanceOf(Test1)
+	expect(a).not.toBeInstanceOf(Test2)
+
+	expect(b).toBeInstanceOf(Test1)
+	expect(b).toBeInstanceOf(Test2)
+
+})
