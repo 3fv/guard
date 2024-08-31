508fda35fbb1441bc5d569039570a685048b6b71
Jonathan Glanz <jonathan.glanz@gmail.com>
2024-08-31 16:35:23 -0400

    wip

--- jest.config.js
+++ jest.config.js
@@ -4,17 +4,14 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 module.exports = {
     verbose: true,
     testMatch: ["**/lib/cjs/**/*.spec.js"],
-    // testRegex: ".*lib\\/cjs\\/.*\\.spec\\.js$",
-
+    
     moduleDirectories: [
       "node_modules",
-      
       "lib/cjs"
     ],
     transform: {
       //".*\\.ts": "@swc-node/jest"
       // ...tsjPreset.transform,
-      
     },
     moduleFileExtensions: [
       "ts",
